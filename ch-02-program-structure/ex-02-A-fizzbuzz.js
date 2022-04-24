f535cee83f9c3837a03cfc9f03d64ba4bdcd0047
Brian Abbott <hi@brianabbott.com>
2022-04-23 21:05:26 -0400

    code ex-02-A. have not checked it yet

---
+++ ch-02-program-structure/ex-02-A-fizzbuzz.js
@@ -0,0 +1,21 @@
+/****************************************************************************************
+ * @Chapter 02
+ * @Exercise 02 A
+ * @Title FIZZBUZZ
+ * @Instructions Write a program that uses console.log to print all the numbers from
+ * 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead.
+ * For numbers divisible by 5 (and not 3) print "Buzz" instead.
+ ***************************************************************************************/
+
+for (let i = 0; i < 100; i++) {
+  // assign a value which will go from 1 to 100 chronologically to the output binding
+  let output = i + 1
+  // if that number is divisible by 3, change output to 'Fizz'
+  output = (output % 3 === 0) ? 'Fizz' : output
+  // if output is still a number, it is not divisible by 3...
+  if (typeof output === 'number')
+    // ...determine if it is divisible by 5
+    output = (output % 5 === 0) ? 'Buzz' : output
+  // write whatever value you ended up with
+  console.log(output)
+}
